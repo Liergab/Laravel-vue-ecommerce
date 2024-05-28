d5958c0769066cfd99e5c2807a385edb601a4e61
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-05-28 13:31:09 +0800

    Address feature

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -13,48 +13,22 @@ class AddressController extends Controller
 {
 
     public function __construct(public AddressServices $addressServices){
-
     }
     
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return $this->addressServices->index();
     }
-
- 
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreAddressRequest $request)
     {
         return $this->addressServices->store($request);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Address $address)
-    {
-        //
-    }
-
- 
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateAddressRequest $request, Address $address)
     {
         return $this->addressServices->update($request, $address);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Address $address)
     {
         return $this->addressServices->destroy($address);
